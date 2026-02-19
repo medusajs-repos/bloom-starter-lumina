7e7bd8ae078485cb793621dd9d48de9106182ae4
olivermrbl <oliver@mrbltech.com>
2026-02-19 09:41:47 +0100

    chore: Add s3/r2 file plugin

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -45,11 +45,28 @@ module.exports = defineConfig({
       options: {
         providers: [
           {
-            resolve: "@medusajs/medusa/file-local",
-            id: "local",
-            options: {
-              upload_dir: "/tmp/medusa-uploads",
-              backend_url: "https://sb-7z30nygbrw59.ai.staging.medusajs.cloud/uploads",
+            id: "s3",
+            resolve: "@medusajs/medusa/file-s3",
+            is_default: true,
+            options: process.env.R2_FILE_URL ? {
+              file_url: process.env.R2_FILE_URL,
+              prefix: process.env.R2_PREFIX,
+              bucket: process.env.R2_BUCKET,
+              endpoint: process.env.R2_ENDPOINT,
+              access_key_id: process.env.R2_ACCESS_KEY_ID,
+              secret_access_key: process.env.R2_SECRET_ACCESS_KEY,
+              region: "auto",
+              additional_client_config: {
+                forcePathStyle: false,
+                requestChecksumCalculation: "WHEN_REQUIRED",
+              },
+            } : {
+              authentication_method: "s3-iam-role",
+              file_url: process.env.S3_FILE_URL,
+              prefix: process.env.S3_PREFIX,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+              region: process.env.S3_REGION,
             },
           },
         ],
